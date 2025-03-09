e2737f328e54aaba7519a4be769ec7b43c6c0cb4
Ahmadjonov Abduboriy <ahmadjonovabduboriy3@gmail.com>
2025-03-10 00:43:49 +0500

    Note about setting component type

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,4 +1,11 @@
 export default function Tabs({ children, buttons, ButtonsContainer }) {
+  /** If we wanna set container values as built in component
+   * like div, menu or etc we use it as string like this
+   * ButtonsContainer="menu"
+   * But if we wanna use our custom component we need to use like this
+   * ButtonsContainer={CustomComponent}
+   */
+
   // const ButtonsContainer = buttonsContainer;
 
   return (
