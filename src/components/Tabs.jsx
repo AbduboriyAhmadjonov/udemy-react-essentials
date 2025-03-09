5d3dbf182d0ef9195b772e718ee116c631c30039
Ahmadjonov Abduboriy <ahmadjonovabduboriy3@gmail.com>
2025-03-10 01:27:58 +0500

    Finishing this project

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,9 +1,13 @@
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
   /** If we wanna set container values as built in component
    * like div, menu or etc we use it as string like this
    * ButtonsContainer="menu"
+   *
    * But if we wanna use our custom component we need to use like this
    * ButtonsContainer={CustomComponent}
+   *
+   * Or we can use it by default cause most of the time we need just menu element
+   * ButtonsContainer = 'menu'
    */
 
   // const ButtonsContainer = buttonsContainer;
