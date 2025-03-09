60b78d3ae4ff0246e777741e1ff48401fc88a022
Ahmadjonov Abduboriy <ahmadjonovabduboriy3@gmail.com>
2025-03-10 01:37:25 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import React from 'react';
-
-import Header from './components/Header/Header.jsx';
-import CoreConcepts from './components/CoreConcepts.jsx';
-import Examples from './components/Examples.jsx';
-
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <CoreConcepts />
-        <Examples />
-      </main>
-    </>
-  );
+  return <h1>React Tic-Tac-Toe</h1>;
 }
 
 export default App;
